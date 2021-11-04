b83eb3587f9694143df53937fa8e8e0fddcbc9b6
U-Vin Lee <w2011@e-mirim.hs.kr>
2021-11-04 10:14:02 +0900

    Add files via upload

---
+++ studyC2021/char18/ex18-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+
+	fp = fopen("C:\studyC2021\a.txt", "r");
+	if (fp == NULL)
+	{
+		printf("파일이 열리지 않았습니다.\n");
+		return 1;
+	}
+	printf("파일이 열렸습니다.\n");
+	fclose(fp);
+
+	return 0;
+}
